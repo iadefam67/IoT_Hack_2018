1b7f20c25849a36d0836af1e3e7e86740a49beb4
Genevieve Curry <genevievecurry@Genevieves-MacBook-Pro.local>
2018-03-24 15:23:52 -0700

    Updated current status + index.html

--- app/js/current-status.js
+++ app/js/current-status.js
@@ -4,11 +4,19 @@ console.log("current-status: ok");
 
 function airNowAPI(){
   var apiURL = 'http://0.0.0.0:3000/api/aqi'
-  $.getJSON(apiURL, function(data){
-    //return cb(null, data.current_observation.display_location.city);
-
-    console.log(data);
+  $.ajax({
+   url: apiURL,
+   dataType: 'json',
+   success: function( data ) {
+     console.log( 'SUCCESS: ', data );
+   },
+   error: function( data ) {
+     console.log( 'ERROR: ', data );
+   }
   });
+
+
+
 }
 
 
