418f832e56f981639c2e3ffc7f784e884f60bc9d
Null <null@David-Bryans-MacBook-Pro.local>
2018-03-24 15:55:17 -0700

    led test is debugged and running, needs cleanup and integration with main

--- Mood_ring_v1/led_test/led_test.ino
+++ Mood_ring_v1/led_test/led_test.ino
@@ -6,6 +6,9 @@ int redPin = 4;
 int greenPin = 5;
 int bluePin = 2;
  
+
+int range;
+  
 //uncomment this line if using a Common Anode LED
 #define COMMON_ANODE
  
@@ -18,36 +21,56 @@ void setup()
  
 void loop()
 
-//take a sample
-//if sample is in a range, assign to r value (maybe another switch table)
 {
+  for (int i = 0; i < 6; ++i) {
+      range = i;
+      delay(1000);    
+  }
+
+
+//  digitalWrite(redPin, 1);
+//  digitalWrite(greenPin, 0);
+//  digitalWrite(bluePin, 1);
+    analogWrite(redPin, 0);
+    analogWrite(greenPin, 1023);
+    analogWrite(bluePin, 1023);
+ 
   switch (range) {
    
-    case r0:
+    case 0:
       //Good: Green 
-      setColor(0, 255, 0);
+      setColor(0, 1023, 0);
       delay(1000);
       break;
       
-    case r1:
-      //Moderate, yellow
-      setColor(255, 255, 0);  
+    case 1:
+      //Moderate: Yellow
+      setColor(1023, 1023, 0);  
       delay(1000);
       break;
       
-    case r2:
-      //Unhealthy for Sensitive Groups (USG) 
-      // orange
+    case 2:
+      //Unhealthy for Sensitive Groups (USG): Orange
+      setColor(1023, 300, 100);
+      delay(1000);
       break;
-      
-    case r3:
-      //Very Unhealthy
-      setColor(80, 0, 80);  // purple
+
+    case 3:
+      //Unhealthy: Red
+      setColor(1023, 0, 0);
       delay(1000);
       break;
       
-    case r4:
-      //Hazardous 
+    case 4:
+      //Very Unhealthy: Purple
+      setColor(900, 0, 1023); 
+      delay(1000);
+      break;
+      
+    case 5:
+      //Hazardous
+      setColor(1023, 0, 1023);
+      delay(1000);
       //darkest possible color
       break;
     default:
@@ -61,9 +84,9 @@ void loop()
 void setColor(int red, int green, int blue)
 {
   #ifdef COMMON_ANODE
-    red = 255 - red;
-    green = 255 - green;
-    blue = 255 - blue;
+    red = 1023 - red;
+    green = 1023 - green;
+    blue = 1023 - blue;
   #endif
   analogWrite(redPin, red);
   analogWrite(greenPin, green);
