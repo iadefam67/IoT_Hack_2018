5bdc6edf717f20255d9f38fef85b9d95061be87c
Null <null@David-Bryans-MacBook-Pro.local>
2018-03-24 13:50:30 -0700

    adding first draft

---
+++ Mood_ring_v1/Mood_ring_v1.ino
@@ -0,0 +1,29 @@
+#define LED LED_BUILTIN
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#define USE_SERIAL Serial
+
+ESP8266WiFiMulti WiFiMulti;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Mood Ring Demo");
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP("SSID", "PASSWORD");
+}
+ 
+void loop() {
+  Serial.print(0);  // To freeze the lower limit
+  Serial.print(" ");
+  Serial.print(1200);  // To freeze the upper limit
+  Serial.print(" ");
+
+  int reading = analogRead(A0);
+  Serial.println(reading);
+  //mapping: 
+//  analogWrite(LED, reading);
+//  delay(10);
+}
